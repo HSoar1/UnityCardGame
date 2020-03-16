c156ebdceb1df18bcc05fef27fdd7e21efe11724
soar1 <soarshizu@gmail.com>
2020-03-16 21:52:24 +0900

    Create_Unity_Project_Framework

---
+++ Assets/Script/CardController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardController : MonoBehaviour
+{
+    
+    CardView view; //見かけに(view)関することの操作
+    CardModel model;//データ(model)に関することの操作
+
+    public void Awakw() {
+        view = GetComponent<CardView>();
+    }
+    
+    public void Init(int cardId) {
+       
+        model = new CardModel(cardId);
+        view.Show(model);
+    }
+}
+
---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    //手札にカード生成
+
+    [SerializeField] Transform playerHandTransform;
+    [SerializeField] CardController cardPrefab;
+    void Start()
+    {
+        CreateCard(playerHandTransform);
+
+    }
+
+    //カードの生成とデータの受け渡し
+    void CreateCard(Transform hand) {
+        CardController card = Instantiate(cardPrefab, hand, false);
+        card.Init(1);//現在はカード1を生成している
+    }
+
+}
